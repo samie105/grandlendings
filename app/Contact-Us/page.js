b8f49be7f04313cf6d6c02ff7e6847df0a1fd2a6
samie105 <samsonrichfield@gmail.com>
2024-02-04 12:13:19 +0100

    real changes

--- app/Contact-Us/page.js
+++ app/Contact-Us/page.js
@@ -1,6 +1,5 @@
 import ContactForm from "@/components/ContactForm";
 import Image from "next/image";
-import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
